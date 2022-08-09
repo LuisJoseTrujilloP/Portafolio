451d5474d12de0e54c5b6e95bfff32034cea7d83
LuisJoseTrujilloP <luisjosetrujillodeveloper@gmail.com>
2022-08-09 14:44:38 -0500

    2022/09/08

--- src/App.js
+++ src/App.js
@@ -1,42 +1,9 @@
-
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import { ThemeProvider } from "styled-components"
-import { lightTheme } from "./components/Themes"
-import GlobalStyle from "./gobalStyles"
-
-import Main from './components/Main';
-import AboutPage from './components/AboutPage';
-import BlogPage from './components/BlogPage';
-import WorkPage from './components/WorkPage';
-import MySkillsPage from './components/MySkillsPage';
-
-
 function App() {
-  return <>
-
-
-<GlobalStyle  />
-
-  <ThemeProvider theme={lightTheme}>
-  <BrowserRouter>
-
-  <Switch>
-
-    <Route exact path="/" component={Main}/>
-    <Route exact path="/about" component={AboutPage}/>
-    <Route exact path="/blog" component={BlogPage}/>
-    <Route exact path="/work" component={WorkPage}/>
-    <Route exact path="/skills" component={MySkillsPage}/>
-
-  </Switch>
-
-  </BrowserRouter>
-
-  </ThemeProvider>
-
-  </>
- 
-
+  return <div>
+    App File
+    </div>
+    
 }
 
 export default App
+
