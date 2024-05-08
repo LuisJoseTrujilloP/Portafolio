34abb2c0c4ed5cb14e15bc41476e3149117caf51
LuisJoseTrujilloP <luisjosetrujillodeveloper@gmail.com>
2024-05-08 12:48:43 -0500

    Changing projects order

--- src/data/WorkData.js
+++ src/data/WorkData.js
@@ -3,25 +3,15 @@
 export const Work = [
     {
         id:1,
-        name:"TodoApp",
-        description:"It is build on top of the React JS, with framer-motion for smooth animations.",
-        tags:["react","framer-motion","Vite"],
+        name:"Ecommerce build with Vue and Tailwind",
+        description:"A react ecommerce app with functional local Registration, checkout menu and more",
+        tags:["React","Vue","Tailwind"],
         
-        demo:"https://luisjosetrujillop.github.io/TodoList/",
-        github:"https://github.com/LuisJoseTrujilloP/TodoList"
+        demo:"https://coruscating-fenglisu-c46173.netlify.app/",
+        github:"https://github.com/LuisJoseTrujilloP/react-vue-ecommerce" 
     },
     {
         id:2,
-        name:"Calculator with JavaScript",
-        description:"A Calculator build in Html Css and Js. With unique functionalities.",
-        tags:["html","css"],
-        
-        demo:"https://jscalculatorltp.netlify.app/",
-        github:"https://github.com/LuisJoseTrujilloP/calculator"
-        
-    },
-    {
-        id:3,
         name:"Spotify Clone with RapidAPI",
         description:"A clone of spotify build using ReactJS. With realtime fetching of articles from dev.to using API.",
         tags:["react","RapidAPI","Tailwind"],
@@ -30,13 +20,21 @@ export const Work = [
         github:"https://github.com/LuisJoseTrujilloP/RapidAPI"
     },
     {
-        id:4,
-        name:"Ecommerce build with Vue and Tailwind",
-        description:"A react ecommerce app with functional local Registration, checkout menu and more",
-        tags:["React","Vue","Tailwind"],
+        id:3,
+        name:"Calculator with JavaScript",
+        description:"A Calculator build in Html Css and Js. With unique functionalities.",
+        tags:["html","css"],
         
-        demo:"https://coruscating-fenglisu-c46173.netlify.app/",
-        github:"https://github.com/LuisJoseTrujilloP/react-vue-ecommerce"
+        demo:"https://jscalculatorltp.netlify.app/",
+        github:"https://github.com/LuisJoseTrujilloP/calculator"
+    },
+    {
+        id:4,
+        name:"TodoApp",
+        description:"It is build on top of the React JS, with framer-motion for smooth animations.",
+        tags:["react","framer-motion","Vite"],
+        demo:"https://luisjosetrujillop.github.io/TodoList/",
+        github:"https://github.com/LuisJoseTrujilloP/TodoList"
     },
   
 ] 
